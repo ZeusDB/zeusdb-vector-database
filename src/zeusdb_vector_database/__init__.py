829d12c7eceaecb653e57c8fc91138f214047edb
doubleinfinity <6169958+doubleinfinity@users.noreply.github.com>
2025-06-20 20:09:45 +1000

    Updated: imports and documentation

--- src/zeusdb_vector_database/__init__.py
+++ src/zeusdb_vector_database/__init__.py
@@ -1,4 +1,7 @@
-__version__ = "0.0.1"
+"""
+ZeusDB Vector Database Module
+"""
+__version__ = "0.0.2"
 
 from .vector_database import VectorDatabase # imports the VectorDatabase class from the vector_database.py file
 
