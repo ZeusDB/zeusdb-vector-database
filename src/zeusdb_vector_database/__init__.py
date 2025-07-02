909f2e1e53861c85436d78537b2ebf2891de20d2
doubleinfinity <6169958+doubleinfinity@users.noreply.github.com>
2025-07-02 21:17:50 +1000

    Bump version to 0.0.4 for development

--- src/zeusdb_vector_database/__init__.py
+++ src/zeusdb_vector_database/__init__.py
@@ -1,7 +1,7 @@
 """
 ZeusDB Vector Database Module
 """
-__version__ = "0.0.3"
+__version__ = "0.0.4"
 
 from .vector_database import VectorDatabase # imports the VectorDatabase class from the vector_database.py file
 
